8cd3fd35b64793aebdf2bdb150564e66f2c1eddc
Patrick GARCIA <pg.soft@free.fr>
2022-10-08 17:11:26 +0200

    fix gen_images

--- gen_images.py
+++ gen_images.py
@@ -1,25 +1,58 @@
 
+# coding: utf-8
+
+'''
+this script generate png image using inkscape like call
+
+steps are :
+- get used image name in styleTclScript.
+- seek in svgAmalgameFile for element-id that is the image name.
+- generate the png in imageDir
+
+module members are :
+- inkscapeCmd     : inkscape like command
+- styleTclScript  : style implemantation script
+- svgAmalgameFile : svg file contaning all image elements with id equals to used image names
+- imageDir        : outpur image directory
+'''
 
 import os
 import re
 
-svgAmalgameFile = os.path.join(os.path.dirname(__file__), 'azure dark', 'assets', 'azure.svg')
-styleTclScript = os.path.join(os.path.dirname(__file__), 'azure dark', 'azure dark.tcl')
-imageDir = os.path.join(os.path.dirname(__file__), 'azure dark', 'images')
+'''inkscape like command'''
+inkscapeCmd = 'inkscape'
+
+'''style implemantation script'''
+styleTclScript = os.path.join(os.path.dirname(__file__), 'azure_dark', 'azure dark.tcl')
+
+'''svg file contaning all image elements with id equals to used image names'''
+svgAmalgameFile = os.path.join(os.path.dirname(__file__), 'azure_dark', 'assets', 'azure.svg')
+
+'''outpur image directory'''
+imageDir = os.path.join(os.path.dirname(__file__), 'azure_dark', 'images')
 
 def osSystem(cmd):
+	'''smart system call'''
 	rc = os.system(cmd)
 	print('rc:', rc, cmd)
 	return rc
 
 def getImageNames():
+	'''seek for image name patter "$I(*)" in styleTclScript'''
 	with open(styleTclScript) as f:
 		return set(re.findall('[$]I\(([^\)]*)\)', f.read()))
 
-def main(inkscapeCmd = 'inkscape'):
+def main():
+	'''start the png image generation'''
 	if not os.path.isdir(imageDir): os.makedirs(imageDir)
 	for imgName in getImageNames():
-		osSystem(inkscapeCmd + ' --file="{1}" --export-id="{0}" --export-png="{2}{0}.png"'.format(imgName, svgAmalgameFile, imageDir + os.sep))
+		osSystem(
+			inkscapeCmd + ' --file="{1}" --export-id="{0}" --export-png="{2}{0}.png"'.format(
+				imgName,
+				svgAmalgameFile,
+				imageDir + os.sep
+			)
+		)
 
 if '__main__' == __name__:
 	main()
